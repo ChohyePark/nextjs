323be090379fc0216a63540c215aa2547c9df93f
park cho hye <chohye0831@gmail.com>
2025-04-08 07:58:40 +0900

    상세 페이지 ui완성

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,18 @@
 import { useRouter } from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 
 export default function Page() {
   const router = useRouter();
   const {q} = router.query;
   return (
-    <>
-      <h1>검색 {q}</h1>
-    </>
+    <div>
+      {
+        books.map((book)=> (<BookItem key={book.id} {...book}/>))
+      }
+    </div>
   );
 }
 
