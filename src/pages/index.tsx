20a6f9bfbbb79750cf9a701421898e607c8c6418
park cho hye <chohye0831@gmail.com>
2025-04-10 18:47:39 +0900

    SSG 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import {InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+// SSR 방식
+// export const getServerSideProps = async () => {
 
-  const [allBooks, randomBooks] = await Promise.all(
+//   const [allBooks, randomBooks] = await Promise.all(
+//     [fetchBooks(), fetchRandomBooks()]
+//   ) // 병렬적으로 동시에 작동
+
+//   return {
+//     props : {
+//       allBooks,
+//       randomBooks
+//     }
+//   }
+// };
+
+// 인덱스 페이지 요청 -> 아래 Home (page) 컴포넌트보다 먼저 실행 
+
+// SSG 방식 
+export const getStaticProps = async () => {
+    const [allBooks, randomBooks] = await Promise.all(
     [fetchBooks(), fetchRandomBooks()]
-  ) // 병렬적으로 동시에 작동
+  ) 
 
   return {
     props : {
@@ -18,12 +34,10 @@ export const getServerSideProps = async () => {
       randomBooks
     }
   }
-};
-
-// 인덱스 페이지 요청 -> 아래 Home (page) 컴포넌트보다 먼저 실행 
+}
 
 
-export default function Home({allBooks,randomBooks} : InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({allBooks,randomBooks} : InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
    <div className={style.container}>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,26 +1,44 @@
-import { useRouter } from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+// import { GetServerSidePropsContext, GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { useEffect, useState } from "react";
+import { BookData } from "@/types";
+import { useRouter } from "next/router";
 
 
-export const getServerSideProps = async (context : GetServerSidePropsContext) => {
+// SSR 방식
+// export const getServerSideProps = async (context : GetServerSidePropsContext) => {
 
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props : {
-      books,
-    },
-  };
-}
+//   return {
+//     props : {
+//       books,
+//     },
+//   };
+// }
 
 
-export default function Page({books} : InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
   const router = useRouter();
-  const {q} = router.query;
+  const q = router.query.q as string;
+
+  // 비동기 함수
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q);
+    setBooks(data);
+  }
+
+  useEffect(()=>{
+    if(q) {
+      fetchSearchResult();
+    }
+  },[q])
+
   return (
     <div>
       {
